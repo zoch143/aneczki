f680078f08d250430bcdf3189f2474c2e18435a5
zoch.143 <zofia_zarnowska@icloud.com>
2026-06-15 18:07:55 +0200

    make correct answer bigger UI

--- server.js
+++ server.js
@@ -363,6 +363,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
